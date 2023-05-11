c916e670c3caf25a1ec9c507f9052c473298a5f4
JeroenvdenB <115401627+JeroenvdenB@users.noreply.github.com>
2023-05-11 16:19:06 +0200

    played with Java packages

---
+++ Account.java
@@ -0,0 +1,7 @@
+package accounting;
+public class Account{
+	private String accountNumber;
+	public static void main(String[] args){
+		System.out.println("You've run the Account class method");
+	}
+}
